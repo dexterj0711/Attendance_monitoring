c56ecf11b967e305a23c2035e6b4ab4c5a505fd5
dexterj0711 <dexterjamesdegayo07.com>
2024-09-02 18:54:28 +0800

    Update adminInstructor.py with latest changes

--- app/routes/adminInstructor.py
+++ app/routes/adminInstructor.py
@@ -1,7 +1,22 @@
-from flask import Blueprint,render_template,redirect,request,session
+from flask import Blueprint,render_template,redirect,request,session,current_app
+import mysql.connector
 
 bp = Blueprint('adminInstructor',__name__,template_folder='templates')
 
-@bp.route('/adminInstructor')
+@bp.route('/adminInstructor', methods = ['POST','GET'])
 def adminInstructor():
+
+    if request.method == 'POST':
+        instructor_name = request.form['instructor_name']
+        department = request.form['department']
+        profile_picture = request.form['profile_picture']
+
+        try:
+            conn = current_app.mysql
+            cursor = conn.cursor()
+            cursor.execute('insert into instructor(instructor_name,department,profile_picture) Values(%s,%s,%s)',(instructor_name,department,profile_picture))
+            conn.commit()
+            return redirect('/adminInstructor')
+        except mysql.connector.Error as e:
+            return f"error: {e}"
     return render_template('adminInstructor.html')
